dac41c86cb350684936ea24353a1ffe57bab9b99
weisunddrei <rudolphburgardt@gmail.com>
2023-09-08 11:13:34 +0500

    init

---
+++ src/test/java/MainClass.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class MainClass {
+    public static int a = 14;
+
+    @Test
+    public void getLocalNumber(){
+        System.out.println(a);
+    }
+//    @Test
+//    public void testGetLocalNumber(){
+//
+//    }
+}
---
+++ src/test/java/MainClassTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class MainClassTest extends MainClass{
+
+    @Test
+    public void testGetLocalNumber(){
+        if (a == 14)
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+}
