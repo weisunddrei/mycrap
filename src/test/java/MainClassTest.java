4ccef42f75f53baf932e1213eaadb5dc73df64dd
weisunddrei <143381725+weisunddrei@users.noreply.github.com>
2023-09-09 12:32:58 +0500

    fix space

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -2,17 +2,18 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class MainClassTest extends MainClass{
-
+    
     @Test
     public void testGetLocalNumber(){
         Assert.assertTrue("Variable A is not equal to 14", firstTest > 14 || firstTest < 14);
     }
-
+    
     @Test
     public void testGetClassNumber() {
         int number = getClassNumber();
         Assert.assertTrue("Number less than 45", number > 45);
     }
+    
     @Test
     public void testGetClassString() {
         String Str = getClassString();
