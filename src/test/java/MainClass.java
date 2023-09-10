7364a07d9c3fbd45a697755291be2bc93805e69e
weisunddrei <rudolphburgardt@gmail.com>
2023-09-10 10:25:49 +0500

    remove @Test in ManClass

--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -6,20 +6,15 @@ public class MainClass {
     private int classNumber = 20;
     private String classString = "Hello, world";
 
-
-    @Test
     public String getClassString(){
-        {return classString;}
+        return classString;
     }
 
-    @Test
     public int getLocalNumber(){
         return firstTest;
     }
 
-    @Test
     public int getClassNumber(){
         return classNumber;
     }
 }
-
