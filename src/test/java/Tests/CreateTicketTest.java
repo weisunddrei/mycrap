6ee305ff3e9b74360ec3e1c9b3c78ca83a082029
rudolph <rudolphburgardt@gmail.com>
2023-10-03 10:31:49 +0500

    add CreateTicketTest

--- src/test/java/Tests/CreateTicketTest.java
+++ src/test/java/Tests/CreateTicketTest.java
@@ -8,38 +8,63 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
-@Features(value = {@Feature(value = "Feedback")})
+@Features(value = {@Feature(value = "Create ticket")})
 public class CreateTicketTest extends CoreTestCase {
 
     public static final String
-            login = "18858",
-            password = "Test20232";
-
+            login = "130522",
+            password = "99679218Aa";
 
 
     @Test
-    @DisplayName("Correct sending feedback")
-    @Description("Filling out fields and sending feedback")
-    @Step("Starting test ID 2 'testSendingFeedback'")
+    @DisplayName("Correct ticket sell create")
+    @Description("Creating a sales request")
+    @Step("Starting test ID 10 'testCreateTicketSell'")
     @Severity(value = SeverityLevel.CRITICAL)
-    public void createTicketSale() throws InterruptedException {
+    public void testCreateTicketSell() {
         AuthPageObject Auth = new AuthPageObject(driver);
         TicketPageObject Ticket = new TicketPageObject(driver);
+        String ticketTypeOperation = "Продать";
+
 
         Auth.authLogin18858(login, password);
 
         Ticket.clickTicketTab();
         Ticket.clickCreateTicketButton();
         Ticket.enterTicketData();
-//        Ticket.enterNameTicket();
-//        Ticket.changeTypeOperation();
-//        Ticket.changeTypeObject();
-//        Ticket.clickCreateTicketButton();
-//        Ticket.asserCreateTicket();
+        if ("Продать".equals(ticketTypeOperation)) {
+            Ticket.changeTicketTypeObjectXpath("Номер");
+        } else {
+            Ticket.changeTicketTypeOperationXpath("Купить");
+            Ticket.changeTicketClassObjectXpath("Гаражи");
+        }
+        Ticket.clickSaveTicketButton();
+        Ticket.assertCreateTicket();
+    }
+
+        @Test
+        @DisplayName("Correct ticket buy create")
+        @Description("Creating a buy request")
+        @Step("Starting test ID 11 'testCreateTicketBuy'")
+        @Severity(value = SeverityLevel.CRITICAL)
+        public void testCreateTicketBuy() {
+            AuthPageObject Auth = new AuthPageObject(driver);
+            TicketPageObject Ticket = new TicketPageObject(driver);
+            String ticketTypeOperation = "Купить";
 
 
+            Auth.authLogin18858(login, password);
 
-
-
+            Ticket.clickTicketTab();
+            Ticket.clickCreateTicketButton();
+            Ticket.enterTicketData();
+            if ("Продать".equals(ticketTypeOperation)) {
+                Ticket.changeTicketTypeObjectXpath("Номер");
+            } else {
+                Ticket.changeTicketTypeOperationXpath("Купить");
+                Ticket.changeTicketClassObjectXpath("Гаражи");
+            }
+            Ticket.clickSaveTicketButton();
+            Ticket.assertCreateTicket();
     }
 }
--- src/test/java/lib/ui/RIES/TicketPageObject.java
+++ src/test/java/lib/ui/RIES/TicketPageObject.java
@@ -7,31 +7,82 @@ public class TicketPageObject extends MainPageObject {
 
     protected static String
             TICKET_TAB = "xpath://*[contains(@text,'Заявки')]",
-    CREATE_TICKET_BUTTON = "id:com.riesapp.debug:id/ticketsListCreateTicketButton",
-    TICKET_PHONE_FIELD = "xpath://*[contains(@text,'Номер телефона')]",
-//    TICKET_PHONE_FIELD_EDIT = "xpath://*[contains(@text,'Номер телефона *')]/following-sibling::*";
-    TICKET_PHONE_FIELD_EDIT = "xpath://*[contains(@text,'Номер телефона *']//*[@resource-id='com.riesapp.debug:id/textFieldEditText']";
+            CREATE_TICKET_BUTTON = "id:com.riesapp.debug:id/ticketsListCreateTicketButton",
+            TICKET_PHONE_FIELD = "xpath://*[contains(@text,'Номер телефона')]",
+            TICKET_PHONE_FIELD_EDIT = "xpath://*[contains(@text,'Номер телефона *')]/preceding-sibling::*",
+            TICKET_CREATE_SAVE_BUTTON = "id:com.riesapp.debug:id/saveButton",
+            TICKET_TYPE_OPERATION = "xpath://*[contains(@text,'Тип операции')]",
+            TICKET_TYPE_OPERATION_TPL = "xpath://*[contains(@text,'{ticketTypeOperation}')]",
+            TICKET_TYPE_OBJECT_TPL = "xpath://*[contains(@text,'{ticketTypeObject}')]",
+            TICKET_TYPE_OBJECT = "xpath://*[contains(@text,'Тип недвижимости')]",
+            TICKET_CLASS_OBJECT = "xpath://*[contains(@text,'Класс недвижимости')]",
+            TICKET_CLASS_OBJECT_TPL = "xpath://*[contains(@text,'{ticketClassObject}')]",
+            APPLICATION_SOURCE = "xpath://*[contains(@text,'Откуда узнали')]";
 
 
-    @Step
+    @Step("Click on ticket tab")
     public void clickTicketTab(){
         this.waitForElementAndClick(TICKET_TAB,"Cannot see and click ticket tab",10);
     }
 
-    @Step
+    @Step("Click on create ticket button")
     public void clickCreateTicketButton(){
         this.waitForElementAndClick(CREATE_TICKET_BUTTON,"Cannot see and click create ticket button",10);
     }
 
-    @Step
-    public void enterTicketData() throws InterruptedException {
+    @Step("Enter data and move to next screen")
+    public void enterTicketData() {
         this.waitForElementAndClick(TICKET_PHONE_FIELD,"Cannot see and click create ticket button",10);
         this.waitForElementAndSendKeys(TICKET_PHONE_FIELD_EDIT,"89827735883","Cannot see and send key create ticket button",10);
-        Thread.sleep(300);
+        this.waitForElementAndClick(TICKET_CREATE_SAVE_BUTTON,"Cannot see and click save button",10);
+        this.waitForElementPresent(APPLICATION_SOURCE, "Cannot see application source", 10);
     }
 
+    @Step("Click on save ticket button")
+    public void clickSaveTicketButton() {
+        this.waitForElementAndClick(TICKET_CREATE_SAVE_BUTTON,"Cannot see and click create save button",10);
+    }
 
+    @Step("Assert correct create ticket")
+    public void assertCreateTicket () {
+        this.waitForElementPresent(APPLICATION_SOURCE, "Cannot see Application Source", 10);
+    }
 
+    @Step("Changing the operation type")
+    public void changeTicketTypeOperationXpath(String ticketTypeOperation) {
+        String ticketTypeOperationXpath = getXpathByTicketType(ticketTypeOperation);
+
+        this.waitForElementAndClick(TICKET_TYPE_OPERATION, "Cannot see and click type operation", 10);
+        this.waitForElementPresent(ticketTypeOperationXpath, "Cannot see needed option", 10);
+        this.waitForElementAndClick(ticketTypeOperationXpath, "Cannot see and click needed option", 10);
+    }
+    private static String getXpathByTicketType(String ticketTypeOperation) {
+        return TICKET_TYPE_OPERATION_TPL.replace("{ticketTypeOperation}", ticketTypeOperation);
+    }
+
+    @Step("Changing the object type")
+    public void changeTicketTypeObjectXpath(String ticketTypeObject) {
+        String ticketTypeObjectXpath = getXpathByTicketTypeObject(ticketTypeObject);
+
+        this.waitForElementAndClick(TICKET_TYPE_OBJECT, "Cannot see and click type object", 10);
+        this.waitForElementPresent(ticketTypeObjectXpath, "Cannot see needed option", 10);
+        this.waitForElementAndClick(ticketTypeObjectXpath, "Cannot see and click needed option", 10);
+    }
+    private static String getXpathByTicketTypeObject(String ticketTypeObject) {
+        return TICKET_TYPE_OBJECT_TPL.replace("{ticketTypeObject}", ticketTypeObject);
+    }
+
+    @Step("Changing the object class")
+    public void changeTicketClassObjectXpath(String ticketClassObject) {
+        String ticketClassObjectXpath = getXpathByTicketClassObject(ticketClassObject);
+
+        this.waitForElementAndClick(TICKET_CLASS_OBJECT, "Cannot see and click class object", 10);
+        this.waitForElementPresent(ticketClassObjectXpath, "Cannot see needed option", 10);
+        this.waitForElementAndClick(ticketClassObjectXpath, "Cannot see and click needed option", 10);
+    }
+    private static String getXpathByTicketClassObject(String ticketClassObject) {
+        return TICKET_CLASS_OBJECT_TPL.replace("{ticketClassObject}", ticketClassObject);
+    }
 
     public TicketPageObject(RemoteWebDriver driver) {
         super(driver);
